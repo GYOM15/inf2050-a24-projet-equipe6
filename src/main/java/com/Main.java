2a2c7d6b9b7b94dea48fdb7b4142ea64a5a55bc9
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2024-10-06 14:02:40 -0400

    Resolution des conflits et Mise à jour du fichier Main.java afin de s'adapter à la nouvelle refactorisation des classes. Elle a été subdivisée en plusieur méthodes afin d'avoir un code plus propre : Méthodes : areArgumentsValid et processFileByType qui sont privée de visibilité et la méthode principale main

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,49 +2,60 @@ package main.java.com;
 
 import main.java.com.Inf2050.Groupe6.Handlers.JsonHandler;
 import main.java.com.Inf2050.Groupe6.Exceptions.Groupe6INF2050Exception;
-import main.java.com.Inf2050.Groupe6.Utilities.JsonFileTreatment;
+import main.java.com.Inf2050.Groupe6.Utilities.JsonFileUtility;
 import main.java.com.Inf2050.Groupe6.Utilities.FileTypeDetermine;
-
 import java.io.IOException;
-
 import static java.lang.System.exit;
 
 public class Main {
+    /**
+     * Point d'entrée principal de l'application; vérifie d'abord la validité des arguments puis procède au traitement du fichier
+     *
+     * @param args Tableau contenant les chemins du fichier d'entrée et de sortie.
+     * @throws Groupe6INF2050Exception Si une erreur survient pendant le traitement du fichier.
+     */
     public static void main(String[] args) throws Groupe6INF2050Exception {
-<<<<<<< HEAD
-        //Vérification du nombre d'arguments que doit prendre notre main en entrée dès le départ.
-=======
->>>>>>> 1e2eb308657b172003cb745d001677b021f7e97d
+        if (!areArgumentsValid(args)) return;
+        try {
+            String fileType = new FileTypeDetermine().determineFileType(args[0]);
+            processFileByType(args, fileType);
+        } catch (IOException e) {
+            System.err.println("Erreur : " + e.getMessage());
+        }
+    }
+
+    /**
+     * Valide le nombre d'arguments passés au programme.
+     *
+     * @param args Tableau contenant les arguments passés à l'application.
+     * @return true si le nombre d'arguments est valide, false sinon.
+     */
+    private static boolean areArgumentsValid(String[] args) {
         if (args.length != 2) {
             System.out.println("Nombre d'arguments invalide");
             exit(-1);
+            return false;
         }
-        // On crée une instance de notre Déterminateur du type de fichier
-        FileTypeDetermine fileTypeDetermine = new FileTypeDetermine();
+        return true;
+    }
 
-        try {
-            // On recupère le fichier d'entrée qui est notre premier élément dans le tableau des paramètres, args[0]
-            String fileType = fileTypeDetermine.determineFileType(args[0]);
-            switch (fileType) {
-                case "application/pdf":
-                    System.out.println("C'est un fichier PDF. Traitement spécifique pour les fichiers PDF.");
-                    break;
 
-                case "application/json":
-                    JsonFileTreatment obj = new JsonFileTreatment(args[0], args[1]);
-                    JsonHandler.handleJson(obj);
-                    break;
-
-                case "text/plain":
-                    System.out.println("C'est un fichier texte. Traitement spécifique pour les fichiers texte.");
-
-                    break;
-
-                default:
-                    System.out.println("Type du fichier non supporté : " + fileType);
+    /**
+     * Gère le traitement des fichiers en fonction du type MIME spécifié. ci-dessous un exemple illustré avec un fichier pdf
+     * Elle peut prendre en charge différents types de fichiers et leur appliquer un traitement spécifique.
+     *
+     * @param args      Tableau contenant les chemins du fichier d'entrée et de sortie.
+     * @param fileType  Le type MIME du fichier (exemple : "application/json" ou "application/pdf").
+     * @throws Groupe6INF2050Exception Si une erreur survient lors du traitement du fichier JSON.
+     */
+    private static void processFileByType(String[] args, String fileType) throws Groupe6INF2050Exception {
+        switch (fileType) {
+            case "application/pdf" -> System.out.println("C'est un fichier PDF. Traitement spécifique pour les fichiers PDF.");
+            case "application/json" -> {
+                JsonFileUtility obj = new JsonFileUtility(args[0], args[1]);
+                JsonHandler.handleJson(obj);
             }
-        } catch (IOException e) {
-            System.err.println("Erreur : " + e.getMessage());
+            default -> System.out.println("Type du fichier non supporté : " + fileType);
         }
     }
 }
