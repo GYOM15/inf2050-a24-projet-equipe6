139ac40cb419514b9874e745aa6bf4837192e26f
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2024-10-06 03:48:04 -0400

    Refactorisation du code : renommage des classes utilitaires (FileTypeDetermine, FileUtility, JsonFileUtility), réduction des méthodes à un maximum de 10 lignes, réorganisation des classes pour une meilleure clarté. Mise à jour de l'énumération ActivityCategory avec les méthodes searchFromJsonCategory(), getMaxHours() et gestion des erreurs via ErrorHandler.

--- src/main/java/com/Inf2050/Groupe6/Enums/ActivityCategory.java
+++ src/main/java/com/Inf2050/Groupe6/Enums/ActivityCategory.java
@@ -4,8 +4,8 @@ import main.java.com.Inf2050.Groupe6.Handlers.ErrorHandler;
 
 
 /**
- * Cette Enumération nous permet d'ajouter de mieux manipuler nos categories à des fins de vérification
- * Chaque catégorie peut avoir une limite d'heures associée ou être sans limite.
+ * Cette Enumération nous permet d'ajouter et de mieux manipuler nos categories à des fins de vérification
+ * Chaque catégorie peut avoir une limite d'heures ou être sans limite.
  * Sources : https://www.jmdoudoux.fr/java/dej/chap-enums.htm
  */
 public enum ActivityCategory {
@@ -27,9 +27,10 @@ public enum ActivityCategory {
     /**
      * Ce constructeur est implémenté pour les catégories qui n'ont aucune limitation d'heures
      *
-     * @param categoryFromJsonObj la catégorie du fichier Json
+     * @param categoryFromJsonObj La catégorie du fichier Json
      * Le deuxième paramètre est mis pour les catégories n'ayant pas le nombre d'heures limitées donc le max des int.
-     * */
+    * */
+
     ActivityCategory(String categoryFromJsonObj) {
         this(categoryFromJsonObj, Integer.MAX_VALUE);
     }
@@ -46,7 +47,7 @@ public enum ActivityCategory {
     }
 
     /** Recupère le nombre d'heures permis pour chaque categorie
-     * @return retourne le maximum d'heures pour chaque catégorie
+     * @return Retourne le maximum d'heures pour chaque catégorie
      * */
     public int getMaxHours() {
         return maxHours;
@@ -56,21 +57,32 @@ public enum ActivityCategory {
      * Recherche dans l'énumération si la catégorie contenu dans l'objet Json, correspond à l'une
      * de celle déclarées dans l'énumération tout en ignorant la casse
      *
-     * @param label qui est la catégorie recupérée de l'objet json à partir de laquelle on vérifie le libélé
+     * @param label Qui est la catégorie recupérée de l'objet json à partir de laquelle on vérifie le libélé
      *              de chaque élément de l'énumération
-     * @param errorHandler enregistre l'erreur si la catégorie n'est pas valide
+     * @param errorHandler Enregistre l'erreur si la catégorie n'est pas valide
      * @return la catégorie concernée si trouvée ou retourne CATEGORIE_NON_VALIDE
      * */
-    public static main.java.com.Inf2050.Groupe6.Enums.ActivityCategory searchFromJsonCategory(String label, ErrorHandler errorHandler) {
-        for (main.java.com.Inf2050.Groupe6.Enums.ActivityCategory category : main.java.com.Inf2050.Groupe6.Enums.ActivityCategory.values()) {
+    public static ActivityCategory searchFromJsonCategory(String label, ErrorHandler errorHandler) {
+        for (ActivityCategory category : ActivityCategory.values()) {
             if (category.categoryFromJsonObj.equalsIgnoreCase(label)) {
                 return category;
             }
         }
-        // Ajoute une erreur si la catégorie n'est pas valide
-        if (errorHandler != null) {
-            errorHandler.addCategoryError("La catégorie " + label+" n'est pas valide");
-        }
+        addErrorIfHandlerPresent(label, errorHandler);
         return CATEGORIE_NON_VALIDE;
+    }
+
+    /**
+     * Cette méthode permet de faire le contrôle avant de rajouter une erreur au gestionnaire d'erreurs lorsqu'une catégorie
+     * n'est pas valide. De ce fait on peut juste faire la vérification sans enregistrer l'erreur, en lui passant null comme valeur
+     *
+     * @param label La catégorie recupérée de l'objet json à partir de laquelle on vérifie le libélé
+     *      *              de chaque élément de l'énumération
+     * @param errorHandler Enregistrer les erreurs. Si null, aucune action n'est effectuée.
+     */
+    private static void addErrorIfHandlerPresent(String label, ErrorHandler errorHandler) {
+        if (errorHandler != null) {
+            errorHandler.addError("La catégorie " + label +" n'est pas valide");
+        }
     }
 }
