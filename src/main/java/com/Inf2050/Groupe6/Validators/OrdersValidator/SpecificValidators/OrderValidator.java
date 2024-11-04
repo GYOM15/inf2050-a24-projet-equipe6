f4f4afb5857e12e63ba5a8870d2cdd14d98d6693
Abdelghani Zebroune <ca591940@ens.uqam.ca>
2024-11-03 20:04:44 -0500

    stash

---
+++ src/main/java/com/Inf2050/Groupe6/Validators/OrdersValidator/SpecificValidators/OrderValidator.java
@@ -0,0 +1,10 @@
+package main.java.com.Inf2050.Groupe6.Validators.OrdersValidator.SpecificValidators;
+
+import main.java.com.Inf2050.Groupe6.Handlers.ErrorHandler;
+import net.sf.json.JSONObject;
+
+public class OrderValidator {
+    public static void validateOrder(JSONObject jsonObject, ErrorHandler errorHandler){
+
+    }
+}
