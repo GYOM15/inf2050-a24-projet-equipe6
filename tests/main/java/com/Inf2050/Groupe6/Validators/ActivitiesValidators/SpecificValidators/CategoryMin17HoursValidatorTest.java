9d5a774029a2d71f9d234bd9db4b0cb37ac08b02
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2024-11-03 19:58:54 -0500

    Rectification de tous les bugs relatifs aux calcul des heures tenant comptes des dates des activités hors cycle, du minimum requis pour certaines catégories précises, et la suppression de la classe CategoryMaxHoursValidator.java permettant de vérifier les heures maximales d'une activités. Egalement changer, la valeur du deuxième paramètres dans le 2eme constructeur de l'énumération afin d'avoir null si aucun maximum n'est spécifié

--- tests/main/java/com/Inf2050/Groupe6/Validators/ActivitiesValidators/SpecificValidators/CategoryMin17HoursValidatorTest.java
+++ tests/main/java/com/Inf2050/Groupe6/Validators/ActivitiesValidators/SpecificValidators/CategoryMin17HoursValidatorTest.java
@@ -1,84 +1,46 @@
 package main.java.com.Inf2050.Groupe6.Validators.ActivitiesValidators.SpecificValidators;
 
 import main.java.com.Inf2050.Groupe6.Handlers.ErrorHandler;
+import net.sf.json.JSON;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CategoryMin17HoursValidatorTest {
+
+    private JSONObject jsonObject;
     private ErrorHandler errorHandler;
-    private JSONObject jsonObject, activity1, activity2;
-    private JSONArray activities;
 
     @BeforeEach
     public void setUp() {
-        errorHandler = new ErrorHandler();
-        activities = new JSONArray();
-        jsonObject =new JSONObject();
-        activity1 = new JSONObject();
-        activity2 =new JSONObject();
+        JSON json = new JSONObject();
+        ErrorHandler errorHandler = new ErrorHandler();
     }
 
     @Test
-    public void testIsCategoryMin17Hours_ValidHours() {
-        activity1.put("categorie", "cours");
-        activity1.put("heures", 10);
-        activities.add(activity1);
-        activity2.put("categorie", "atelier");
-        activity2.put("heures", 10);
-        activities.add(activity2);
-        jsonObject.put("activites", activities);
-        jsonObject.put("heures_transferees_du_cycle_precedent", 5);
-        CategoryMin17HoursValidator.isCategoryMin17Hours(jsonObject, errorHandler);
-        assertEquals(0, errorHandler.getErrors().size());
-    }
-
-    @Test
-    public void testIsCategoryMin17Hours_InvalidHours() {
-        activity1.put("categorie", "cours");
-        activity1.put("heures", 5);
-        activities.add(activity1);
-        activity2.put("categorie", "atelier");
-        activity2.put("heures", 5);
-        activities.add(activity2);
-        jsonObject.put("activites", activities);
+    public void testValidateMin17_WithSufficientHours() {
         jsonObject.put("heures_transferees_du_cycle_precedent", 0);
-        CategoryMin17HoursValidator.isCategoryMin17Hours(jsonObject, errorHandler);
-        assertEquals(1, errorHandler.getErrors().size());
-        assertEquals("Il manque 7 heures de formation dans les catégories suivantes : cours, atelier", errorHandler.getErrors().get(0));
+        jsonObject.put("activites", createActivites(10,5, "cours", "atelier"));
+
+        int totalHours =
     }
 
-    @Test
-    public void testIsCategoryMin17Hours_ExcessivelyLowHours() {
-        activity1.put("categorie", "cours");
-        activity1.put("heures", 1);
-        activities.add(activity1);
-        activity2.put("categorie", "atelier");
-        activity2.put("heures", 1);
-        activities.add(activity2);
-        jsonObject.put("activites", activities);
-        jsonObject.put("heures_transferees_du_cycle_precedent", 0);
-        CategoryMin17HoursValidator.isCategoryMin17Hours(jsonObject, errorHandler);
-        assertEquals(1, errorHandler.getErrors().size());
-        assertEquals("Il manque 15 heures de formation dans les catégories suivantes : cours, atelier", errorHandler.getErrors().get(0));
+    private JSONArray createActivites(int ... hoursAndCategories) {
+        JSONArray activites = new JSONArray();
+        for (int i = 0; i < hoursAndCategories.length; i +=2) {
+            int hours = hoursAndCategories[i];
+            int category = hoursAndCategories[i+1];
+            activites.add(createActivity(hours, category));
+        }
+        return activites;
     }
 
-    @Test
-    public void testIsCategoryMin17Hours_MixedCategories() {
-        activity1.put("categorie", "cours");
-        activity1.put("heures", 10);
-        activities.add(activity1);
-        activity2.put("categorie", "non_concerne");
-        activity2.put("heures", 5);
-        activities.add(activity2);
-        jsonObject.put("activites", activities);
-        jsonObject.put("heures_transferees_du_cycle_precedent", 0);
-        CategoryMin17HoursValidator.isCategoryMin17Hours(jsonObject, errorHandler);
-        assertEquals(1, errorHandler.getErrors().size());
-        assertEquals("Il manque 7 heures de formation dans les catégories suivantes : cours", errorHandler.getErrors().get(0));
+    private JSONObject createActivity(int hours, int category) {
+        JSONObject activity = new JSONObject();
+        activity.put("date", "2024-04-10");
+        activity.put("hours", hours);
+        activity.put("category", category);
+        return activity;
     }
-
 }
