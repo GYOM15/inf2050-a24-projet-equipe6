170e2470cccc23abd9d12e47f36c15d88cd7b429
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2024-11-05 19:01:21 -0500

    ajout de la validation des dates avant le calcul des heures

--- src/main/java/com/Inf2050/Groupe6/Validators/ActivitiesValidators/SpecificValidators/DateValidator.java
+++ src/main/java/com/Inf2050/Groupe6/Validators/ActivitiesValidators/SpecificValidators/DateValidator.java
@@ -4,7 +4,6 @@ import main.java.com.Inf2050.Groupe6.Handlers.ErrorHandler;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
-<<<<<<< HEAD
 public class DateValidator{
     /**
      * Permet de parser les dates et renvoie une exception, gère automatiquement les années bissextiles .
@@ -20,25 +19,5 @@ public class DateValidator{
             ErrorHandler.addErrorIfNotNull(errorHandler,"Format incorrect ou année non bisextille : " + dateValue);
             return false;
         }
-=======
-public class DateValidator {
-
-    public static LocalDate validateDate(String dateValue, ErrorHandler errorHandler) {
-        LocalDate date = null;
-        try {
-            date = LocalDate.parse(dateValue);
-            if (date.getMonthValue() == 2 && date.getDayOfMonth() == 29 && !date.isLeapYear()) {
-                if (errorHandler != null) {
-                    errorHandler.addError("Date d'activité invalide : " + dateValue + " (année non bissextile)");
-                }
-                return null;
-            }
-        } catch (DateTimeParseException e) {
-            if (errorHandler != null) {
-                errorHandler.addError("Date d'activité invalide ou format incorrect (attendu: yyyy-MM-dd) : " + dateValue);
-            }
-        }
-        return date;
->>>>>>> 473f43b (On stash la validation du cycle)
     }
 }
