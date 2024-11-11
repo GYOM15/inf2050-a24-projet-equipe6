ced23eeb7b3f55be1802bf9233a25c6ba6a27ea1
Abdelghani Zebroune <ca591940@ens.uqam.ca>
2024-11-10 23:50:54 -0500

    Ajout des modifications dans le répertoire src/main/java/com/ : code source

---
+++ src/main/java/com/Inf2050/Groupe6/Validators/HoursCalculators/OrdersHoursTotalCalculators/ArchitectesTotalHoursValidator.java
@@ -0,0 +1,26 @@
+package com.Inf2050.Groupe6.Validators.HoursCalculators.OrdersHoursTotalCalculators;
+
+
+import org.example.Inf2050.Groupe6.Enums.Cycle;
+import org.example.Inf2050.Groupe6.Handlers.ErrorHandler;
+
+public class ArchitectesTotalHoursValidator{
+
+    /**
+     * Valide si le total d'heures accumulées pour les architectes est suffisant pour le cycle donné.
+     *
+     * @param cycle Le cycle d'évaluation (ex. : CYCLE_2023_2025)
+     * @param totalHours Le nombre total d'heures accumulées par l'architecte
+     * @param errorHandler L'instance ErrorHandler pour enregistrer les erreurs éventuelles
+     */
+    public static void validateByCycle(Cycle cycle, int totalHours, ErrorHandler errorHandler) {
+        int requiredHours = switch (cycle) {
+            case CYCLE_2023_2025 -> 40;
+            case CYCLE_2020_2022, CYCLE_2018_2020 -> 42;
+            default -> 0;
+        };
+        if (totalHours < requiredHours && requiredHours > 0) {
+            ErrorHandler.addErrorIfNotNull(errorHandler,"Il manque " + (requiredHours - totalHours) + " heures pour compléter le cycle " + cycle.getLabel());
+        }
+    }
+}
---
+++ src/main/java/com/Inf2050/Groupe6/Validators/HoursCalculators/OrdersHoursTotalCalculators/GeologueTotalHoursValidator.java
@@ -0,0 +1,23 @@
+package com.Inf2050.Groupe6.Validators.HoursCalculators.OrdersHoursTotalCalculators;
+
+
+import org.example.Inf2050.Groupe6.Enums.Cycle;
+import org.example.Inf2050.Groupe6.Handlers.ErrorHandler;
+
+public class GeologueTotalHoursValidator {
+
+    /**
+     * Valide si le total d'heures accumulées pour les géologues est suffisant pour compléter le cycle donné.
+     *
+     * @param cycle Le cycle d'évaluation (ex. : CYCLE_2023_2025)
+     * @param totalHours Le nombre total d'heures accumulées par le géologue
+     * @param errorHandler L'instance ErrorHandler pour enregistrer les erreurs éventuelles
+     */
+    public static void validateByCycle(Cycle cycle, int totalHours, ErrorHandler errorHandler) {
+        int requiredHours = 55;
+        if (totalHours < requiredHours) {
+            ErrorHandler.addErrorIfNotNull(errorHandler,"Il manque " + (requiredHours - totalHours) +
+                    " heures pour compléter le cycle " + cycle.getLabel());
+        }
+    }
+}
