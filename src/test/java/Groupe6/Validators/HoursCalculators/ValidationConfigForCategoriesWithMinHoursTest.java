8ba47805430bf74fa7a26e7999b63a0f63b2f1ed
Abdelghani Zebroune <ca591940@ens.uqam.ca>
2024-11-10 23:50:20 -0500

    Ajout de tests pour ValidationConfigForCategoriesWithMinHours : validation des catégories avec heures minimales

--- src/test/java/Groupe6/Validators/HoursCalculators/ValidationConfigForCategoriesWithMinHoursTest.java
+++ src/test/java/Groupe6/Validators/HoursCalculators/ValidationConfigForCategoriesWithMinHoursTest.java
@@ -1,5 +1,6 @@
 package Groupe6.Validators.HoursCalculators;
 
+import org.example.Inf2050.Groupe6.Validators.HoursCalculators.ValidationConfigForCategoriesWithMinHours;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
