e2f915f31bae8ce51d443a41020bdb8427a9236f
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2024-12-08 17:18:12 -0500

    Remise des tests après construction de l'artefact pour la validation des resultats

---
+++ src/test/java/GROUPE6_INF2050/Utilities/FileUtilityTest.java
@@ -0,0 +1,27 @@
+package GROUPE6_INF2050.Utilities;
+
+
+import GROUPE6_INF2050.Exceptions.Groupe6INF2050Exception;
+import GROUPE6_INF2050.Utilities.FileUtility;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FileUtilityTest {
+
+    private final String inputFile = "tests/main/Vendor/inputTest.json";
+    private final String outputFile = "tests/main/Vendor/outputTest.json";
+
+    @Test
+    void testConstructor() {
+        FileUtility fileUtility = new FileUtility(inputFile, outputFile) {
+            @Override
+            public void loadAndValid() throws Groupe6INF2050Exception {
+            }
+        };
+        assertEquals(inputFile, fileUtility.inputFile);
+        assertEquals(outputFile, fileUtility.outputFile);
+    }
+
+
+}
