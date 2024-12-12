105c70c9eda61706c427422ea393c24e5e9487d8
Abdelghani Zebroune <ca591940@ens.uqam.ca>
2024-12-11 22:48:12 -0500

    Mis à jour les tests pour FileUtility.

--- src/test/java/GROUPE6_INF2050/Utilities/FileUtilityTest.java
+++ src/test/java/GROUPE6_INF2050/Utilities/FileUtilityTest.java
@@ -2,25 +2,24 @@ package GROUPE6_INF2050.Utilities;
 
 
 import GROUPE6_INF2050.Exceptions.Groupe6INF2050Exception;
-import GROUPE6_INF2050.Utilities.FileUtility;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FileUtilityTest {
 
-    private final String inputFile = "tests/main/Vendor/inputTest.json";
-    private final String outputFile = "tests/main/Vendor/outputTest.json";
+    private final String INPUT_FILE = "src/test/java/resources/FileUtilityTest/inputFileTest_Default.json";
+    private final String OUTPUT_FILE = "src/test/java/resources/outputFileTest.json";
 
     @Test
     void testConstructor() {
-        FileUtility fileUtility = new FileUtility(inputFile, outputFile) {
+        FileUtility fileUtility = new FileUtility(INPUT_FILE, OUTPUT_FILE) {
             @Override
             public void loadAndValid() throws Groupe6INF2050Exception {
             }
         };
-        assertEquals(inputFile, fileUtility.inputFile);
-        assertEquals(outputFile, fileUtility.outputFile);
+        assertEquals(INPUT_FILE, fileUtility.inputFile);
+        assertEquals(OUTPUT_FILE, fileUtility.outputFile);
     }
 
 
